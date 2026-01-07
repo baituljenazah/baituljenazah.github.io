007272071f766902a56d9f1aa9c0869c8a20a31c
baituljannah <seasaltfloat@gmail.com>
2026-01-07 12:13:46 +0800

    Update and rename direktorijs.js to direktori.js

--- js/direktori.js
+++ js/direktori.js
@@ -1,4 +1,4 @@
-        document.addEventListener('DOMContentLoaded', function() {
+            document.addEventListener('DOMContentLoaded', function() {
             // DOM Elements
             const mobileMenuBtn = document.getElementById('mobileMenuBtn');
             const mobileMenuOverlay = document.getElementById('mobileMenuOverlay');
